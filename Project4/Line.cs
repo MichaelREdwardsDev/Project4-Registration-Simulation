563c09f455d7dd7b4d9e87b7b11c12ea7e16ac38
Michael Edwards <EDWARDSMR@ETSU.EDU>
2019-11-14 20:06:23 -0500

    Can I commit to master though?

---
+++ Project4/Line.cs
@@ -0,0 +1,49 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Project:		Project 4 - Registration Simulation
+//	File Name:		Line.cs
+//	Description:	Holds a Priority Queue as well as an ID allowing for ease of differentiations between lines
+//	Course:			CSCI 2210-001 - Data Structures
+//	Author:			Michael Edwards, edwardsmr@etsu.edu
+//	Created:		Sunday November 14, 2019
+//	Copyright:		Michael Edwards, 2019
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4 {
+	/// <summary>
+	/// Encapsulates the PriorityQueue, as well as it's Count and ID for ease of differentiation between lines
+	/// </summary>
+	class Line {
+		/// <summary>
+		/// The queue containing the registrants already in this particular line
+		/// </summary>
+		public PriorityQueue<Registrant> Registrants { get; set; } = new PriorityQueue<Registrant>();
+		/// <summary>
+		/// The number of registrants in the line
+		/// </summary>
+		public int Count { get {
+				return Registrants.Count;
+			}
+		}
+		/// <summary>
+		/// The ID for a particular line, allows ease of differentiation between lines
+		/// </summary>
+		public int LineID { get; set; }
+		/// <summary>
+		/// Line Overloaded Constructor - Takes the ID for a line particular line
+		/// </summary>
+		/// <param name="lineID"></param>
+		public Line(int lineID) {
+			LineID = lineID;
+		}
+		public void Enqueue(Registrant reg) {
+			Registrants.Enqueue(reg);
+		}
+	}
+}
