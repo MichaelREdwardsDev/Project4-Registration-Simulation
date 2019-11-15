90351686d0adff5d190b41fbfb457e6f8b4690b4
Michael Edwards <EDWARDSMR@ETSU.EDU>
2019-11-14 19:37:18 -0500

    Project Added

---
+++ Project4/Line.cs
@@ -0,0 +1,36 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Project:		Project 4 - Registration Simulation
+//	File Name:		Line.cs
+//	Description:	Holds a Priority Queue as well as an ID allowing for ease of differentiations between lines
+//	Course:			CSCI 2210-001 - Data Structures
+//	Author:			Michael Edwards, edwardsmr@etsu.edu
+//	Created:		Sunday November 14, 2019
+//	Copyright:		Michael Edwards, 2019
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4 {
+	/// <summary>
+	/// Encapsulates the PriorityQueue, as well as it's Count and ID for ease of differentiation between lines
+	/// </summary>
+	class Line {
+		public PriorityQueue<Registrant> Registrants { get; set; } = new PriorityQueue<Registrant>();
+		public int Count { get {
+				return Registrants.Count;
+			}
+		}
+		public int LineID { get; set; }
+		public Line(int lineID) {
+			LineID = lineID;
+		}
+		public void Enqueue(Registrant reg) {
+			Registrants.Enqueue(reg);
+		}
+	}
+}
